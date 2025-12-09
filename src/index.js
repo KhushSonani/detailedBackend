31f92cabba8e9e82d675ac91860ef666038ca7a9
Khush Sonani <khushsonani0512@gmail.com>
2025-12-09 20:31:55 +0530

    Util code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 // require("dotenv").config({path:"./env"});
 
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path:"./env"
-}); 
+  path: "./env",
+});
 
-connectDB();
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongodb connection failed !!", err);
+  });
 
 // import mongoose from "mongoose";
 // import {DB_NAME} from './constants.js';
-// Approach 1   
+// Approach 1
 //ifi
 /*
 ;(async ()=>{
