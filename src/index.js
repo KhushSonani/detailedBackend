9f1987ef02ad5821a07f1902c35f5ccbde19c32f
Khush Sonani <khushsonani0512@gmail.com>
2025-12-10 21:30:05 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
@@ -41,4 +42,3 @@ connectDB()
     }
 })()
 */
-// Approach 2
