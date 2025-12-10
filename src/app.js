8052f0a47a4ca020bfefd1269d649d61f11fbcff
Khush Sonani <khushsonani0512@gmail.com>
2025-12-10 18:36:10 +0530

    completed production grade set up for backend

--- src/app.js
+++ src/app.js
@@ -6,15 +6,15 @@ const app = express();
 // app.use(cors());
 // if want settings for cors
 app.use(
-  cors({ 
+  cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
 
 app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({extended:true,limit: "16kb"}))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(cookieParser());   
+app.use(cookieParser());
 
-export {app} 
+export { app };
