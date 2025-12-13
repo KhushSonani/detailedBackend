0052f77cbdaa1febf2ccf39745922e61b978335f
Khush Sonani <khushsonani0512@gmail.com>
2025-12-13 17:51:59 +0530

    Refactor subscription model for improved readability and consistency

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,14 +1,17 @@
-import {mongoose,Schema} from "mongoose";
+import { mongoose, Schema } from "mongoose";
 
-const subscriptionSchema = new Schema({
-    subscriber :{
-        type : Schema.Types.ObjectId, // one who is subscribing
-        ref:"User",
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId, // one who is subscribing
+      ref: "User",
     },
-    channel :{
-        type : Schema.Types.ObjectId, // one who is being subscribed
-        ref:"User",
-    }
-},{timestamps:true});
+    channel: {
+      type: Schema.Types.ObjectId, // one who is being subscribed
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema);
